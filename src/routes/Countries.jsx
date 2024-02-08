67696e53aadda20a2eee007ff402dba7bea587a7
Martin Holland <martin.holland@free.fi>
2024-02-08 22:44:29 +0200

    added search function and link to single page

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { Spinner } from "react-bootstrap";
+import { Form, Spinner } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { initializeCountries } from "../store/countriesSlice";
 import { addFavourite } from "../store/favouritesSlice";
 
@@ -16,6 +16,7 @@ const Countries = () => {
 
   const countriesList = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.isLoading);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     dispatch(initializeCountries());
@@ -38,51 +39,75 @@ const Countries = () => {
 
   return (
     <Container fluid>
+      <Row>
+        <Col className="text-center m-5">
+          <Form>
+            <Form.Control
+              style={{ width: "18rem" }}
+              type="search"
+              className="me-2 "
+              placeholder="Search for countries"
+              aria-label="Search"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </Form>
+        </Col>
+      </Row>
       <Row xs={2} md={3} lg={4} className=" g-3">
-        {countriesList.map((country) => (
-          <Col key={country.name.official} className="mt-5">
-            <Card className="h-100">
-              <FavoriteIcon
-                color="red"
-                onClick={() => dispatch(addFavourite(country))}
-              />
-              <Card.Img
-                variant="top"
-                className="rounded h-50"
-                src={country.flags.svg}
-                style={{
-                  objectFit: "cover",
-                  minHeight: "200px",
-                  maxHeight: "200px",
-                }}
-              />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title>{country.name.common}</Card.Title>
-                <Card.Subtitle className="mb-5 text-muted">
-                  {country.name.official}
-                </Card.Subtitle>
-                <ListGroup
-                  variant="flush"
-                  className="flex-grow-1 justify-content-end"
-                >
-                  <ListGroup.Item>
-                    <i className="bi bi-translate me-2"></i>
-                    {Object.values(country.languages ?? {}).join(", ")}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <i className="bi bi-cash-coin me-2"></i>
-                    {Object.values(country.currencies || {})
-                      .map((currency) => currency.name)
-                      .join(", ")}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    {country.population.toLocaleString()}
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        {countriesList
+          .filter((c) => {
+            return c.name.common.toLowerCase().includes(search.toLowerCase());
+          })
+          .map((country) => (
+            <Col className="mt-5" key={country.name.common}>
+              <Link
+                to={`/countries/${country.name.common}`}
+                state={{ country: country }}
+              >
+                <Card className="h-100">
+                  <i
+                    className="bi bi-heart-fill text-danger m-1 p-1"
+                    onClick={() => dispatch(addFavourite(country.name.common))}
+                  ></i>
+                  <Card.Img
+                    variant="top"
+                    src={country.flags.svg}
+                    className="rounded h-50"
+                    style={{
+                      objectFit: "cover",
+                      minHeight: "200px",
+                      maxHeight: "200px",
+                    }}
+                  />
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{country.name.common}</Card.Title>
+                    <Card.Subtitle className="mb-5 text-muted">
+                      {country.name.official}
+                    </Card.Subtitle>
+                    <ListGroup
+                      variant="flush"
+                      className="flex-grow-1 justify-content-end"
+                    >
+                      <ListGroup.Item>
+                        <i className="bi bi-translate me-2"></i>
+                        {Object.values(country.languages ?? {}).join(", ")}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <i className="bi bi-cash-coin me-2"></i>
+                        {Object.values(country.currencies || {})
+                          .map((currency) => currency.name)
+                          .join(", ")}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <i className="bi bi-people me-2"></i>
+                        {country.population.toLocaleString()}
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          ))}
       </Row>
     </Container>
   );
